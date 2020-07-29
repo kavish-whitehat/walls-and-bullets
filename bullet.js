2216ca422aeccecf6a9bb5bd1e87511924fe6a0a
kavish <67267883+kavish-whitehat@users.noreply.github.com>
2020-07-29 15:48:02 +0530

    Add files via upload

---
+++ bullet.js
@@ -0,0 +1,11 @@
+class Bullet extends BaseClass{
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("sprites/bullet.png");
+    }
+    display(){
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    }
+  }
---
+++ wall.js
@@ -0,0 +1,11 @@
+class wall extends BaseClass{
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("sprites/wall.png");
+    }
+    display(){
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    }
+  }
